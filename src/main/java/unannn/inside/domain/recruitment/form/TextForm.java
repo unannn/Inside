9c046e57e63d80ba9d9b62d227a7793f25ef455d
이윤환 <zoo7930@sju.ac.kr>
2022-05-03 22:48:27 +0900

    생성자 추가

--- src/main/java/unannn/inside/domain/recruitment/form/TextForm.java
+++ src/main/java/unannn/inside/domain/recruitment/form/TextForm.java
@@ -1,10 +1,15 @@
 package unannn.inside.domain.recruitment.form;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import unannn.inside.domain.recruitment.Question;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @DiscriminatorValue("TEXT")
 @Entity
 public class TextForm  extends Question {
