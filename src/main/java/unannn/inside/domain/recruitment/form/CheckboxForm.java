12537a7eea9230f07f43f0cad9ea2e793b8b90f0
이윤환 <zoo7930@sju.ac.kr>
2022-05-03 22:47:56 +0900

    Element들 매핑하는 생성자 추가

--- src/main/java/unannn/inside/domain/recruitment/form/CheckboxForm.java
+++ src/main/java/unannn/inside/domain/recruitment/form/CheckboxForm.java
@@ -1,5 +1,7 @@
 package unannn.inside.domain.recruitment.form;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import unannn.inside.domain.recruitment.Question;
 
 import javax.persistence.DiscriminatorValue;
@@ -8,10 +10,17 @@ import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 @DiscriminatorValue("CHECKBOX")
+@NoArgsConstructor
 @Entity
 public class CheckboxForm extends Question {
     @OneToMany(mappedBy = "checkboxForm")
     private List<CheckboxElement> elements = new ArrayList<>();
 
+    public CheckboxForm(List<String> elementNames) {
+        for (String elementName : elementNames) {
+            elements.add(new CheckboxElement(elementName));
+        }
+    }
 }
--- src/main/java/unannn/inside/domain/recruitment/form/SelectForm.java
+++ src/main/java/unannn/inside/domain/recruitment/form/SelectForm.java
@@ -1,7 +1,11 @@
 package unannn.inside.domain.recruitment.form;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import unannn.inside.domain.recruitment.Question;
 
+import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
@@ -9,9 +13,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 @DiscriminatorValue("SELECT")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @Entity
 public class SelectForm extends Question {
 
-    @OneToMany(mappedBy = "selectForm")
+    @OneToMany(mappedBy = "selectForm",cascade = CascadeType.ALL)
     private List<SelectElement> elements = new ArrayList<>();
+
+    public SelectForm(List<String> elementNames) {
+        for (String elementName : elementNames) {
+            elements.add(new SelectElement(elementName));
+        }
+    }
 }
