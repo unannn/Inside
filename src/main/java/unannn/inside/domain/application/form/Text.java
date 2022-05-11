99606ef43ab22999e1949713512f2f7dfa6e9a0f
이윤환 <zoo7930@sju.ac.kr>
2022-05-11 23:54:25 +0900

    질문 번호 설정을 위해 super() 추가

--- src/main/java/unannn/inside/domain/application/form/Text.java
+++ src/main/java/unannn/inside/domain/application/form/Text.java
@@ -1,12 +1,19 @@
 package unannn.inside.domain.application.form;
 
+import lombok.NoArgsConstructor;
 import unannn.inside.domain.application.Answer;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
+@NoArgsConstructor
 @DiscriminatorValue("TEXT")
 @Entity
 public class Text extends Answer {
     private String content;
+
+    public Text(Integer  questionNumber, String content) {
+        super(questionNumber);
+        this.content = content;
+    }
 }
