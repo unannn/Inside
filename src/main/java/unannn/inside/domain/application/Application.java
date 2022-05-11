9e30803134ab859cabdd4cb694e8ba2590901868
이윤환 <zoo7930@sju.ac.kr>
2022-05-11 23:55:56 +0900

    지원서 글 추가 편의 메서드 추가

--- src/main/java/unannn/inside/domain/application/Application.java
+++ src/main/java/unannn/inside/domain/application/Application.java
@@ -1,7 +1,9 @@
 package unannn.inside.domain.application;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import unannn.inside.domain.recruitment.Recruitment;
 import unannn.inside.domain.user.User;
 
@@ -9,7 +11,7 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Application {
@@ -25,7 +27,15 @@ public class Application {
     @JoinColumn(name="recruitment_id")
     private Recruitment recruitment;
 
-    @OneToMany(mappedBy = "application")
+    @OneToMany(mappedBy = "application", cascade = CascadeType.PERSIST)
     private List<Answer> answers = new ArrayList<>();
+
+    public void storeAnswer(Answer answer) {
+        if (answer.getApplication() != null) {
+            answer.getApplication().getAnswers().remove(answer);
+        }
+        answer.setApplication(this);
+        answers.add(answer);
+    }
 
 }
