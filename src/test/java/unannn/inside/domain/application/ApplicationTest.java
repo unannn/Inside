fa9649cdbc74059f8cac5695b1b4ba2d050d3dfe
이윤환 <zoo7930@sju.ac.kr>
2022-05-12 23:53:55 +0900

    Application 답변 유형 별 저장 테스트

--- src/test/java/unannn/inside/domain/application/ApplicationTest.java
+++ src/test/java/unannn/inside/domain/application/ApplicationTest.java
@@ -5,13 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import unannn.inside.domain.application.form.Checkbox;
+import unannn.inside.domain.application.form.CheckboxValue;
+import unannn.inside.domain.application.form.Select;
 import unannn.inside.domain.application.form.Text;
 import unannn.inside.domain.recruitment.Question;
 import unannn.inside.domain.recruitment.Recruitment;
+import unannn.inside.domain.recruitment.form.CheckboxForm;
+import unannn.inside.domain.recruitment.form.SelectForm;
 import unannn.inside.domain.recruitment.form.TextForm;
 import unannn.inside.domain.user.User;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -24,7 +30,7 @@ class ApplicationTest {
     private TestEntityManager em;
 
     @Test
-    public void Text_Answer_저장() throws Exception {
+    public void Text_저장() throws Exception {
         //given
         User user = getTestUser();
         Recruitment recruitment = getTestRecruitment();
@@ -55,6 +61,82 @@ class ApplicationTest {
         assertThat(answer.getQuestionNumber()).isEqualTo(textQuestion.getQuestionNumber());
     }
 
+
+    @Test
+    public void CheckBox_Answer_저장() throws Exception {
+        //given
+        User user = getTestUser();
+        Recruitment recruitment = getTestRecruitment();
+        CheckboxForm checkboxQuestion = (CheckboxForm) getCheckboxQuestion();
+        recruitment.addQuestion(checkboxQuestion);
+        user.register(recruitment);
+
+        String content = "안녕하세요, unannn 입니다.";
+        Application application = new Application();
+
+        List<String> elements = List.of("수박");
+        Checkbox checkbox = new Checkbox(checkboxQuestion.getQuestionNumber(), elements);
+
+        //when
+        user.register(application);
+
+        application.storeAnswer(checkbox);
+        em.persist(application);
+        em.flush();
+        em.clear();
+
+        //then
+        Application findApplication = em.find(Application.class, application.getId());
+        assertThat(findApplication.getId()).isEqualTo(application.getId());
+
+        List<Answer> answers = findApplication.getAnswers();
+        Answer answer = answers.stream().findFirst().orElse(null);
+        System.out.println(answer);
+        assertThat(answer).isNotNull();
+        assertThat(answer.getId()).isEqualTo(checkbox.getId());
+
+        assertThat(answer.getQuestionNumber()).isEqualTo(checkboxQuestion.getQuestionNumber());
+
+        //Application 영속화할 때 CACADE를 통해 User 엔티티도 업데이트
+        assertThat(user.getApplications().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void Select_Answer_저장() throws Exception {
+        //given
+        User user = getTestUser();
+        Recruitment recruitment = getTestRecruitment();
+        SelectForm selectQuestion = (SelectForm) getSelectQuestion();
+        recruitment.addQuestion(selectQuestion);
+        user.register(recruitment);
+
+        Application application = new Application();
+
+        Select select = new Select(selectQuestion.getQuestionNumber(), "남자");
+
+        //when
+        application.storeAnswer(select);
+        user.register(application);
+        em.persist(application);
+        em.flush();
+        em.clear();
+
+        //then
+        Application findApplication = em.find(Application.class, application.getId());
+        assertThat(findApplication.getId()).isEqualTo(application.getId());
+
+        List<Answer> answers = findApplication.getAnswers();
+        Answer answer = answers.stream().findFirst().orElse(null);
+        System.out.println(answer);
+        assertThat(answer).isNotNull();
+        assertThat(answer.getId()).isEqualTo(select.getId());
+
+        assertThat(answer.getQuestionNumber()).isEqualTo(selectQuestion.getQuestionNumber());
+
+        //Application 영속화할 때 CACADE를 통해 User 엔티티도 업데이트
+        assertThat(user.getApplications().size()).isEqualTo(1);
+    }
+
     private Recruitment getTestRecruitment() {
         Recruitment recruitment = Recruitment.builder()
                 .title("과식동아리 모집")
@@ -70,6 +152,26 @@ class ApplicationTest {
         question.setQuestion(1,"이름이 뭐에요?");
         return question;
     }
+
+    private Question getCheckboxQuestion() {
+        List<String> elements = new ArrayList<>();
+        elements.add("수박");
+        elements.add("사과");
+        elements.add("키위");
+        Question question = new CheckboxForm(elements);
+        question.setQuestion(1,"올해 먹은 과일 선택");
+        return question;
+    }
+
+    private Question getSelectQuestion() {
+        List<String> elementNames = new ArrayList<>();
+        elementNames.add("남자");
+        elementNames.add("여자");
+        Question question = new SelectForm(elementNames);
+        question.setQuestion(1,"성별?");
+        return question;
+    }
+
 
     private User getTestUser() {
         return User.builder()
