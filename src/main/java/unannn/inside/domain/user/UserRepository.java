bd7d05f899e821614f66da7bdf2a11fe45263619
이윤환 <zoo7930@sju.ac.kr>
2022-05-19 23:30:04 +0900

    username으로 찾기 메소드 추가

--- src/main/java/unannn/inside/domain/user/UserRepository.java
+++ src/main/java/unannn/inside/domain/user/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import unannn.inside.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    public User findByUsername(String username);
 }
