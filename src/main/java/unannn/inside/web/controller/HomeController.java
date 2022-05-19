6eaf6490a1127b1ef885a88060deb4403b0830a7
이윤환 <zoo7930@sju.ac.kr>
2022-05-19 23:30:18 +0900

    HomeController 생성

--- src/main/java/unannn/inside/web/controller/HomeController.java
+++ src/main/java/unannn/inside/web/controller/HomeController.java
@@ -1,2 +1,27 @@
-package unannn.inside.web.controller;public class HomeController {
+package unannn.inside.web.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import unannn.inside.config.auth.PrincipalDetails;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        if(principalDetails == null){
+            return "redirect:/login";
+        }
+        return "redirect:/user";
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "loginForm";
+    }
+
 }
