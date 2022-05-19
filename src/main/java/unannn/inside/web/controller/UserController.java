149371cc1181b2430f62a0ad72e69060500e66cd
이윤환 <zoo7930@sju.ac.kr>
2022-05-19 23:30:39 +0900

    회원가입 컨트롤러 생성

--- src/main/java/unannn/inside/web/controller/UserController.java
+++ src/main/java/unannn/inside/web/controller/UserController.java
@@ -1,13 +1,51 @@
 package unannn.inside.web.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import unannn.inside.config.auth.PrincipalDetails;
+import unannn.inside.domain.user.User;
+import unannn.inside.domain.user.UserRepository;
+import unannn.inside.web.dto.JoinDto;
 
-@Controller("/user")
+@RequiredArgsConstructor
+@RequestMapping("/user")
+@Controller
 public class UserController {
 
-    @GetMapping("/login")
-    public String loginForm() {
-        return "loginForm";
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @GetMapping
+    public String user(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        System.out.println(principalDetails.getUsername());
+        return "userMain";
+    }
+
+    @GetMapping("/join")
+    public String joinForm(Model model) {
+        model.addAttribute("joinDto", new JoinDto());
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(JoinDto joinDto) {
+        //검증 로직 수행
+
+        User user = User.builder()
+                .username(joinDto.getUsername())
+                .encodedPassword(bCryptPasswordEncoder.encode(joinDto.getPassword()))
+                .email(joinDto.getEmail())
+                .phoneNumber(joinDto.getPhoneNumber())
+                .build();
+
+
+        userRepository.save(user);
+
+        return "redirect:/login";
     }
 }
