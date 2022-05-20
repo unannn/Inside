1b24b28dd0d02581adfab7c910e4ef61054658de
이윤환 <zoo7930@sju.ac.kr>
2022-05-20 23:44:44 +0900

    회원가입 검증 로직 추가

--- src/main/java/unannn/inside/web/controller/UserController.java
+++ src/main/java/unannn/inside/web/controller/UserController.java
@@ -1,10 +1,12 @@
 package unannn.inside.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,9 @@ import unannn.inside.domain.user.User;
 import unannn.inside.domain.user.UserRepository;
 import unannn.inside.web.dto.JoinDto;
 
+import javax.validation.Valid;
+
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/user")
 @Controller
@@ -33,7 +38,17 @@ public class UserController {
     }
 
     @PostMapping("/join")
-    public String join(JoinDto joinDto) {
+    public String join(@Valid JoinDto joinDto, BindingResult bindingResult) {
+
+        if(!joinDto.getPassword().equals(joinDto.getVerifyPassword())){
+            bindingResult.reject("NotSamePassword");
+        }
+
+        if(bindingResult.hasErrors()){
+            log.debug("errors = {}", bindingResult.getAllErrors());
+            return "joinForm";
+        }
+
         //검증 로직 수행
 
         User user = User.builder()
