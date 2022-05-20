cbcac3d3f2189f480034e388a70c176174b2670e
이윤환 <zoo7930@sju.ac.kr>
2022-05-20 23:45:49 +0900

    검증 어노테이션 추가

--- src/main/java/unannn/inside/web/dto/JoinDto.java
+++ src/main/java/unannn/inside/web/dto/JoinDto.java
@@ -3,13 +3,30 @@ package unannn.inside.web.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import unannn.inside.web.valid.Tel;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class JoinDto {
+
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String password;
+
+    @NotBlank
     private String verifyPassword;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @Tel
     private String phoneNumber;
 }
