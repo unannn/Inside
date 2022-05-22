643d574f8ee56baea829cd50cbc0eb675f149336
이윤환 <zoo7930@sju.ac.kr>
2022-05-22 23:20:29 +0900

    @Transactional ReadOnly 속성 추가

--- src/main/java/unannn/inside/domain/user/UserRepository.java
+++ src/main/java/unannn/inside/domain/user/UserRepository.java
@@ -1,8 +1,11 @@
 package unannn.inside.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 import unannn.inside.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Transactional(readOnly = true)
     public User findByUsername(String username);
 }
