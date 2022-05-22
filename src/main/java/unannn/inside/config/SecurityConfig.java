5353c91d7e6b519bd5485f6207083e5888384add
이윤환 <zoo7930@sju.ac.kr>
2022-05-22 23:20:01 +0900

    로그인 실패 처리 핸들러 등록

--- src/main/java/unannn/inside/config/SecurityConfig.java
+++ src/main/java/unannn/inside/config/SecurityConfig.java
@@ -7,11 +7,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import unannn.inside.config.auth.LoginFailedHandler;
 
 @RequiredArgsConstructor
 @EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터체인에 등록이 됩니다.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,17 +31,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login") //권한 없이 권한이 필요한 페이지로 접근시 해당 페이지 GET 요청
-                .loginProcessingUrl("/login") //login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행
-                .defaultSuccessUrl("/user") //로그인이 완료되면 이돌될 url
+                .loginProcessingUrl("/login") // /login 으로 POST 요청 시 시큐리티가 낚아채서 대신 로그인을 진행
+                .defaultSuccessUrl("/user") //로그인이 완료되면 이동할 url
+                .failureHandler(new LoginFailedHandler())
 
-                /**
-                 * //구글 로그인이 완료된 뒤의 후처리 필요.
-                 * 1. 코드 받기(인증),
-                 * 2. 엑세스 토큰(권한),
-                 * 3. 사용자 프로필정보를 가져오고
-                 * 4. 그 정보를 토대로 회원가입을 진행시키기도 함
-                 * 4-2 (이메일, 전화번호, 이름, 아이디 등) 만약 쇼핑몰이라면 집주소, 백화점 몰, vip등급, 일반등급
-                 */
+        /**
+         * //구글 로그인이 완료된 뒤의 후처리 필요.
+         * 1. 코드 받기(인증),
+         * 2. 엑세스 토큰(권한),
+         * 3. 사용자 프로필정보를 가져오고
+         * 4. 그 정보를 토대로 회원가입을 진행시키기도 함
+         * 4-2 (이메일, 전화번호, 이름, 아이디 등) 만약 쇼핑몰이라면 집주소, 백화점 몰, vip등급, 일반등급
+         */
 //                .and()
 //                .oauth2Login()
 //                .loginPage("/loginForm")  //구글 로그인을 하면 엑세스 토큰 + 사용자 프로필을 한번에 받음
