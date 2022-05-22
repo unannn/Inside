fc57041c7bb09f978405c18cc6a88e2da85a9dd3
이윤환 <zoo7930@sju.ac.kr>
2022-05-22 23:20:42 +0900

    로그인 실패 처리 컨트롤러 추가

--- src/main/java/unannn/inside/web/controller/HomeController.java
+++ src/main/java/unannn/inside/web/controller/HomeController.java
@@ -2,11 +2,19 @@ package unannn.inside.web.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import unannn.inside.config.auth.PrincipalDetails;
+import unannn.inside.web.dto.LoginDto;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -20,7 +28,18 @@ public class HomeController {
     }
 
     @GetMapping("/login")
-    public String loginForm() {
+    public String loginForm(Model model, HttpServletRequest request) {
+        model.addAttribute("loginDto", new LoginDto());
+        return "loginForm";
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute LoginDto loginDto, BindingResult bindingResult, HttpServletRequest request, Model model) {
+        model.addAttribute("loginDto", loginDto);
+        if(!loginDto.getUsername().isEmpty() && !loginDto.getPassword().isEmpty()){
+            bindingResult.reject("InvalidAccount");
+            return "loginForm";
+        }
         return "loginForm";
     }
 
