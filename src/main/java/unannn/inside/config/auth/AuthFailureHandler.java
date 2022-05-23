a719b05c68857b01583e149f27eee1239d5869a6
이윤환 <zoo7930@sju.ac.kr>
2022-05-23 22:43:36 +0900

    인증 핸들러 이름 변경

--- src/main/java/unannn/inside/config/auth/AuthFailureHandler.java
+++ src/main/java/unannn/inside/config/auth/AuthFailureHandler.java
@@ -3,7 +3,6 @@ package unannn.inside.config.auth;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import unannn.inside.web.dto.LoginDto;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -11,14 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
-public class LoginFailedHandler implements AuthenticationFailureHandler {
+public class AuthFailureHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        String username = (String) request.getParameter("username");
-        String password = (String) request.getParameter("password");
-//        request.setAttribute("loginDto", new LoginDto(username, password));
-        request.getParameterMap().entrySet().forEach(o1 -> System.out.println(o1.getKey() +  " = " + o1.getValue()[0]));
-//        request.setAttribute("loginDto",new LoginDto(username,password));
         request.getRequestDispatcher("/login").forward(request, response);
     }
 }
