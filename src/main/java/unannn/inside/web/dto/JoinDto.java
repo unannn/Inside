d591f83b4a7aabd0ac398c517025d4d6b7643137
이윤환 <zoo7930@sju.ac.kr>
2022-05-23 22:50:26 +0900

    @NotBlank 제거

--- src/main/java/unannn/inside/web/dto/JoinDto.java
+++ src/main/java/unannn/inside/web/dto/JoinDto.java
@@ -11,12 +11,10 @@ import javax.validation.constraints.*;
 @Setter
 public class JoinDto {
 
-    @NotBlank
-    @Pattern(regexp = "[a-z\\d_-]{5,20}", message = "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 가능합니다")
+    @Pattern(regexp = "[a-z\\d_-]{5,20}")
     private String username;
 
-    @NotBlank
-    @Pattern(regexp = "[a-z\\d!@#$%^&*_+=-]{8,20}", message = "8~20자의 영문 소문자, 숫자와 특수기호(!@#$%^&*_-+=)만 가능합니다.")
+    @Pattern(regexp = "[a-z\\d!@#$%^&*_+=-]{8,20}")
     private String password;
 
     @NotBlank
