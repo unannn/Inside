da79cb61577e363a5adfdddb71d1fe250f4a54df
이윤환 <zoo7930@sju.ac.kr>
2022-05-23 22:46:55 +0900

    로그인 입력값 검증 필터 등록

---
+++ src/main/java/unannn/inside/config/WebConfig.java
@@ -0,0 +1,22 @@
+package unannn.inside.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import unannn.inside.web.filter.LoginValidationFilter;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean LoginValidationFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginValidationFilter());
+        filterRegistrationBean.setOrder(-101);
+        filterRegistrationBean.addUrlPatterns("/login");
+        return filterRegistrationBean;
+    }
+}
