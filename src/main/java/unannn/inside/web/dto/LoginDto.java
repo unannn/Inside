607d25cc0dfe1e1ddcf795abbdb6f8b9dfacb65b
이윤환 <zoo7930@sju.ac.kr>
2022-05-25 18:13:57 +0900

    @NoArgumentConstructor 제거
    매개변수가 없는 생성자가 있으면 setter를 통해 @ModelAttribute 객체를 바인딩 하는데, setter를 만들지 않기 위해 제거하였다

--- src/main/java/unannn/inside/web/dto/LoginDto.java
+++ src/main/java/unannn/inside/web/dto/LoginDto.java
@@ -1,12 +1,14 @@
 package unannn.inside.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
-@NoArgsConstructor
 @Getter
+@AllArgsConstructor
 public class LoginDto {
     @NotBlank
     private String username;
@@ -14,8 +16,4 @@ public class LoginDto {
     @NotBlank
     private String password;
 
-    public LoginDto(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
 }
