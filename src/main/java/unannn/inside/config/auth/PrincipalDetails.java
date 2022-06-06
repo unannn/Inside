1509b25d7566122c7f9a79a3c1c37ad3af755c9a
이윤환 <zoo7930@sju.ac.kr>
2022-06-06 23:55:23 +0900

    OAuth2User 상속

--- src/main/java/unannn/inside/config/auth/PrincipalDetails.java
+++ src/main/java/unannn/inside/config/auth/PrincipalDetails.java
@@ -1,20 +1,31 @@
 package unannn.inside.config.auth;
 
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import unannn.inside.domain.user.User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
-public class PrincipalDetails implements UserDetails {
+@Getter
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private final User user;
+    private Map<String,Object> attributes;
 
+    //일반로그인
     public PrincipalDetails(User user) {
         this.user = user;
     }
 
+    //OAuth 로그인
+    public PrincipalDetails(User user, Map<String,Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Collection<GrantedAuthority> authorities = new ArrayList<>();
@@ -56,5 +67,15 @@ public class PrincipalDetails implements UserDetails {
         //사이트에서 1년동안 회원이 로그인을 한해서 휴면 계정으로 하기로한다면
         // 현재시간 - 최근 로그인 시간이 1년을 초과하면 return false
         return true;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
