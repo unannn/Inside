b61d09bd268576fecfbbb37ede9af512620e32e5
이윤환 <zoo7930@sju.ac.kr>
2022-06-08 23:10:09 +0900

    회원가입 컨트롤러 userController에서 HomeController 로 이동

--- src/main/java/unannn/inside/web/controller/HomeController.java
+++ src/main/java/unannn/inside/web/controller/HomeController.java
@@ -1,11 +1,20 @@
 package unannn.inside.web.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import unannn.inside.config.auth.PrincipalDetails;
+import unannn.inside.domain.user.User;
+import unannn.inside.domain.user.UserRepository;
+import unannn.inside.web.dto.JoinDto;
 import unannn.inside.web.dto.LoginDto;
 
 import javax.servlet.ServletRequest;
@@ -14,8 +23,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+@Slf4j
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping("/")
     public String home(@AuthenticationPrincipal PrincipalDetails principalDetails) {
@@ -38,5 +52,36 @@ public class HomeController {
             bindingResult.reject("InvalidAccount");
         }
         return "loginForm";
+    }
+
+    @GetMapping("/join")
+    public String joinForm(Model model) {
+        model.addAttribute("joinDto", new JoinDto());
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(@Valid JoinDto joinDto, BindingResult bindingResult, Model model) {
+
+        if(!joinDto.getPassword().equals(joinDto.getVerifyPassword())){
+            bindingResult.reject("NotSamePassword");
+        }
+
+        if(bindingResult.hasErrors()){
+            log.debug("errors = {}", bindingResult.getAllErrors());
+            return "joinForm";
+        }
+
+        User user = User.builder()
+                .username(joinDto.getUsername())
+                .name(joinDto.getName())
+                .encodedPassword(bCryptPasswordEncoder.encode(joinDto.getPassword()))
+                .email(joinDto.getEmail())
+                .build();
+
+
+        userRepository.save(user);
+
+        return "redirect:/login";
     }
 }
