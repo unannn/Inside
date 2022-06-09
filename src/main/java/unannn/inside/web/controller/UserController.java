81ac14cf32dda579a6fd9938f7fe75cac529a7c9
이윤환 <zoo7930@sju.ac.kr>
2022-06-10 01:55:01 +0900

    principalDetails 에 준영속상태의 User Entity를 merger() 통해 영속화

--- src/main/java/unannn/inside/web/controller/UserController.java
+++ src/main/java/unannn/inside/web/controller/UserController.java
@@ -5,28 +5,42 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import unannn.inside.config.auth.PrincipalDetails;
+import unannn.inside.domain.application.Application;
+import unannn.inside.domain.recruitment.Recruitment;
 import unannn.inside.domain.user.User;
 import unannn.inside.domain.user.UserRepository;
 import unannn.inside.web.dto.JoinDto;
 
+import javax.persistence.EntityManager;
 import javax.validation.Valid;
+import java.util.List;
 
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/user")
 @Controller
 public class UserController {
 
+    private final UserRepository userRepository;
 
+    private final EntityManager em;
+
+    @Transactional
     @GetMapping
     public String userMain(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
-        model.addAttribute("name",principalDetails.getName());
-        model.addAttribute("applications", principalDetails.getUser().getApplications());
-        model.addAttribute("recruitments", principalDetails.getUser().getRecruitments());
+
+        User loginUser = em.merge(principalDetails.getUser()); //준영속 상태의 엔티티를 다시 영속상태로 변경
+
+        model.addAttribute("name", loginUser.getName());
+        model.addAttribute("applications", loginUser.getApplications());
+        model.addAttribute("recruitments", loginUser.getRecruitments());
         return "userMain";
     }
 
