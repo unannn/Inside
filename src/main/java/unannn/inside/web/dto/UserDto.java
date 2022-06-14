d2feb439257d501d3d109184a88fc269cb67f70d
이윤환 <zoo7930@sju.ac.kr>
2022-06-15 02:33:07 +0900

    유저 메인페이지용 dto 생성

---
+++ src/main/java/unannn/inside/web/dto/UserDto.java
@@ -0,0 +1,25 @@
+package unannn.inside.web.dto;
+
+import lombok.Builder;
+import unannn.inside.domain.application.Application;
+import unannn.inside.domain.recruitment.Recruitment;
+import unannn.inside.domain.user.User;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDto {
+
+    private String name;
+
+    private List<Recruitment> recruitments;
+
+    private List<Application> applications;
+
+    public UserDto(User user) {
+        this.name = user.getName();
+        this.applications = user.getApplications();
+        this.recruitments = user.getRecruitments();
+    }
+}
