dfd0bbb709e1deb6133ea177a6ed2a8bd1f102ea
이윤환 <zoo7930@sju.ac.kr>
2022-06-27 17:56:17 +0900

    User가 공고를 동적생성하기위한 API 컨트롤러 생성

---
+++ src/main/java/unannn/inside/web/controller/UserApiController.java
@@ -0,0 +1,38 @@
+package unannn.inside.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import unannn.inside.config.auth.PrincipalDetails;
+import unannn.inside.domain.recruitment.Recruitment;
+import unannn.inside.domain.user.User;
+import unannn.inside.web.dto.recruitment.RecruitmentDto;
+import unannn.inside.web.service.UserService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class UserApiController {
+
+    private final UserService userService;
+
+    @GetMapping("/api/recruitments")
+    public List<RecruitmentDto> userRecruitmentList(@AuthenticationPrincipal PrincipalDetails userDetails) {
+        log.debug("userService = {}",userService);
+        log.debug("userDetails = {}",userDetails);
+        return userService.getUserRecruitments(userDetails.getUser());
+    }
+
+    @PostMapping("/api/recruitments/new")
+    public List<RecruitmentDto> newRecruitment(@AuthenticationPrincipal PrincipalDetails userDetails) {
+        return userService.addRecruitment(userDetails.getUser());
+    }
+}
