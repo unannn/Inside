8e79f13a13d96693e42acd4e574cefe82c1f3303
이윤환 <zoo7930@sju.ac.kr>
2022-06-27 17:55:41 +0900

    공고 생성 컨트롤러 분리

--- src/main/java/unannn/inside/web/controller/RecruitmentController.java
+++ src/main/java/unannn/inside/web/controller/RecruitmentController.java
@@ -2,16 +2,41 @@ package unannn.inside.web.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import unannn.inside.config.auth.PrincipalDetails;
+import unannn.inside.domain.recruitment.Recruitment;
+import unannn.inside.domain.user.User;
+import unannn.inside.web.dto.recruitment.RecruitmentDto;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
-@Controller("/recruitment")
+@RequestMapping("/recruitments")
+@Controller
 public class RecruitmentController {
 
-    @GetMapping
-    public String createForm() {
+    /*
+     * 모집 공고 수정 페이지
+     * */
+    @GetMapping("/{recruitmentId}/edit")
+    @ResponseBody
+    public String editForm(Model model, @PathVariable UUID recruitmentId) {
+        model.addAttribute("recruitmentDto", new RecruitmentDto());
         return "recruitment/createForm";
+    }
+
+    @PostMapping("/test")
+    public @ResponseBody String test(HttpServletRequest request) {
+        String token = request.getHeader("X-CSRF-TOKEN");
+        log.info("token = {}", token);
+        return "hello jquery";
     }
 }
