201e70ffe02b25f1a5b76acaa09573153c052042
이윤환 <zoo7930@sju.ac.kr>
2022-06-28 23:11:29 +0900

    공고 테스트 UserAPi 테스트로 이동

--- src/test/java/unannn/inside/web/controller/RecruitmentControllerTest.java
+++ src/test/java/unannn/inside/web/controller/RecruitmentControllerTest.java
@@ -1,30 +1,29 @@
 package unannn.inside.web.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultHandler;
+import unannn.inside.web.dto.recruitment.RecruitmentDto;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class RecruitmentControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    void 모집공고생성_페이지() throws Exception {
-        mockMvc.perform(get("/recruitment"))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeExists("recruitmentDto"));
-    }
-
 }
