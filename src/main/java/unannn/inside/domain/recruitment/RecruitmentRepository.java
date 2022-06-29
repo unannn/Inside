eb6387aaafe069e80f89d1fce38bc3a0023c7755
이윤환 <zoo7930@sju.ac.kr>
2022-06-29 22:27:08 +0900

    PK 타입 변경

--- src/main/java/unannn/inside/domain/recruitment/RecruitmentRepository.java
+++ src/main/java/unannn/inside/domain/recruitment/RecruitmentRepository.java
@@ -1,6 +1,12 @@
 package unannn.inside.domain.recruitment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+import unannn.inside.domain.user.User;
 
-public interface RecruitmentRepository extends JpaRepository<Recruitment,Long> {
+import java.util.List;
+import java.util.UUID;
+
+public interface RecruitmentRepository extends JpaRepository<Recruitment, UUID> {
+
 }
