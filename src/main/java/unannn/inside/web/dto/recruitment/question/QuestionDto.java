73920de75312eb736da566648cdc3f2a37690801
이윤환 <zoo7930@sju.ac.kr>
2022-06-30 22:23:17 +0900

    toDto 추상 메서드 추가

--- src/main/java/unannn/inside/web/dto/recruitment/question/QuestionDto.java
+++ src/main/java/unannn/inside/web/dto/recruitment/question/QuestionDto.java
@@ -1,8 +1,13 @@
 package unannn.inside.web.dto.recruitment.question;
 
+import unannn.inside.domain.recruitment.Question;
+import unannn.inside.domain.recruitment.form.TextForm;
+
 import java.util.List;
 
 public abstract class QuestionDto {
     private int questionNumber;
     private String question;
+
+    abstract QuestionDto toDto(Question entity);
 }
