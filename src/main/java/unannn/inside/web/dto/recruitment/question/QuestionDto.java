b9cf922c7033b9233327bd24209b88e40d99fc1a
이윤환 <zoo7930@sju.ac.kr>
2022-07-02 23:40:12 +0900

    QutionsForm 타입 별 형 변환

--- src/main/java/unannn/inside/web/dto/recruitment/question/QuestionDto.java
+++ src/main/java/unannn/inside/web/dto/recruitment/question/QuestionDto.java
@@ -1,13 +1,33 @@
 package unannn.inside.web.dto.recruitment.question;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.jboss.jandex.ClassType;
 import unannn.inside.domain.recruitment.Question;
+import unannn.inside.domain.recruitment.form.CheckboxForm;
+import unannn.inside.domain.recruitment.form.SelectForm;
 import unannn.inside.domain.recruitment.form.TextForm;
 
 import java.util.List;
 
-public abstract class QuestionDto {
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+public class QuestionDto {
     private int questionNumber;
     private String question;
 
-    abstract QuestionDto toDto(Question entity);
+
+    public static QuestionDto toDto(Question entity){
+        QuestionDto questionDto = new QuestionDto(entity.getQuestionNumber(), entity.getQuestion());
+        if(entity instanceof TextForm){
+            TextForm textForm = (TextForm) entity;
+        } else if (entity instanceof CheckboxForm) {
+            CheckboxForm textForm = (CheckboxForm) entity;
+        } else if(entity instanceof SelectForm){
+            SelectForm textForm = (SelectForm) entity;
+        }
+
+        return questionDto;
+    }
 }
