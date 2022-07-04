6b9156a141681e2e2986cec26de3db1f06366b9f
이윤환 <zoo7930@sju.ac.kr>
2022-07-04 22:48:41 +0900

    Question 타입 별 Dto 생성자 구현

--- src/main/java/unannn/inside/web/dto/recruitment/question/SelectDto.java
+++ src/main/java/unannn/inside/web/dto/recruitment/question/SelectDto.java
@@ -1,8 +1,19 @@
 package unannn.inside.web.dto.recruitment.question;
 
+import unannn.inside.domain.recruitment.form.SelectElement;
+import unannn.inside.domain.recruitment.form.SelectForm;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SelectDto extends QuestionDto{
     private List<String> elements;
+
+    SelectDto(SelectForm selectForm) {
+        List<SelectElement> selectElements = selectForm.getElements();
+        elements = selectElements.stream()
+                .map(SelectElement::getElement)
+                .collect(Collectors.toList());
+    }
 
 }
--- src/main/java/unannn/inside/web/dto/recruitment/question/TextDto.java
+++ src/main/java/unannn/inside/web/dto/recruitment/question/TextDto.java
@@ -1,5 +1,16 @@
 package unannn.inside.web.dto.recruitment.question;
 
+import unannn.inside.domain.recruitment.Question;
+import unannn.inside.domain.recruitment.form.TextForm;
+
 public class TextDto extends QuestionDto{
-    private String content;
+    private Integer minimumLetter;
+    private Integer maximumLetter;
+
+    public TextDto(TextForm entity){
+        super(entity.getQuestionNumber(), entity.getQuestion());
+        this.minimumLetter = entity.getMinimumLetter();
+        this.maximumLetter = entity.getMaximumLetter();
+    }
+
 }
