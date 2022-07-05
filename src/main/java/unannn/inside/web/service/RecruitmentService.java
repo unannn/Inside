53d8157e6b379474059fed315e4161211bb38ebb
이윤환 <zoo7930@sju.ac.kr>
2022-07-05 22:02:23 +0900

    질문 타입별로 toDto하는 로직 구현

--- src/main/java/unannn/inside/web/service/RecruitmentService.java
+++ src/main/java/unannn/inside/web/service/RecruitmentService.java
@@ -4,10 +4,12 @@ package unannn.inside.web.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import unannn.inside.domain.recruitment.Question;
 import unannn.inside.domain.recruitment.Recruitment;
 import unannn.inside.domain.recruitment.RecruitmentRepository;
 import unannn.inside.domain.user.User;
 import unannn.inside.web.dto.recruitment.RecruitmentDto;
+import unannn.inside.web.dto.recruitment.question.QuestionDto;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -20,12 +22,20 @@ public class RecruitmentService {
 
     private final RecruitmentRepository recruitmentRepository;
 
-    public RecruitmentDto getRecruitmentForm(UUID recruitmentId) {
+    @Transactional
+    public RecruitmentDto getRecruitmentForm(UUID recruitmentId) throws Exception {
 
         Recruitment findRecruitment = recruitmentRepository.findById(recruitmentId)
                 .orElseThrow(() -> new IllegalArgumentException("모집 공고를 찾을 수 없습니다."));
 
-        return RecruitmentDto.toDto(findRecruitment);
+        RecruitmentDto recruitmentDto = RecruitmentDto.toDto(findRecruitment);
+
+        List<Question> questions = findRecruitment.getQuestions();
+        for (Question question : questions) {
+            recruitmentDto.getQuestionDtos().add(QuestionDto.toDto(question));
+        }
+        
+        return recruitmentDto;
     }
 
 }
