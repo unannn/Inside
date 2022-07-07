063ba694be8c6221993db6f0f9a37e88fa1fec90
이윤환 <zoo7930@sju.ac.kr>
2022-07-07 23:31:58 +0900

    모집 공고에 새 질문 추가 컨트롤러 구현

---
+++ src/main/java/unannn/inside/web/controller/RecruitmentApiController.java
@@ -0,0 +1,27 @@
+package unannn.inside.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import unannn.inside.web.dto.AddQuestionDto;
+import unannn.inside.web.dto.NewQuestionDto;
+import unannn.inside.web.dto.recruitment.QuestionType;
+import unannn.inside.web.dto.recruitment.RecruitmentDto;
+import unannn.inside.web.service.RecruitmentService;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class RecruitmentApiController {
+
+    private final RecruitmentService recruitmentService;
+
+    @PostMapping("/recruitment/question/new")
+    public RecruitmentDto newQuestion(@ModelAttribute NewQuestionDto newQuestionDto) throws Exception {
+        UUID recruitmentId = newQuestionDto.getRecruitmentId();
+        QuestionType questionType = newQuestionDto.getQuestionType();
+        return recruitmentService.addQuestion(recruitmentId, questionType);
+    }
+
+}
