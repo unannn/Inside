23e1a2daeb6a35e8e0ccc7bd7c59f19dd608ad21
이윤환 <zoo7930@sju.ac.kr>
2022-07-08 22:31:21 +0900

    생성자 매개변수에 questionDtos 추가

--- src/main/java/unannn/inside/web/dto/recruitment/question/SelectDto.java
+++ src/main/java/unannn/inside/web/dto/recruitment/question/SelectDto.java
@@ -1,11 +1,15 @@
 package unannn.inside.web.dto.recruitment.question;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import unannn.inside.domain.recruitment.form.SelectElement;
 import unannn.inside.domain.recruitment.form.SelectForm;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Getter
+@NoArgsConstructor
 public class SelectDto extends QuestionDto{
     private List<String> elements;
 
