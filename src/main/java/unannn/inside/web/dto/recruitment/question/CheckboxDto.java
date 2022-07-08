b188418c38ba27b99ae88f6fbc83b430d48e2ecb
이윤환 <zoo7930@sju.ac.kr>
2022-07-08 22:30:49 +0900

    Getter추가

--- src/main/java/unannn/inside/web/dto/recruitment/question/CheckboxDto.java
+++ src/main/java/unannn/inside/web/dto/recruitment/question/CheckboxDto.java
@@ -1,5 +1,7 @@
 package unannn.inside.web.dto.recruitment.question;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import unannn.inside.domain.recruitment.Question;
 import unannn.inside.domain.recruitment.form.CheckboxElement;
 import unannn.inside.domain.recruitment.form.CheckboxForm;
@@ -8,8 +10,10 @@ import unannn.inside.domain.recruitment.form.SelectElement;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Getter
+@NoArgsConstructor
 public class CheckboxDto extends QuestionDto{
-    private final List<String> elements;
+    private List<String> elements;
 
     CheckboxDto(CheckboxForm checkboxForm) {
         List<CheckboxElement> selectElements = checkboxForm.getElements();
